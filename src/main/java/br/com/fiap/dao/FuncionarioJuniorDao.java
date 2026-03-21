23cf65c867cc55c79497ab4fca1bd1d1fd12b7c3
dnl-alm <danielfonseca.almeida@gmail.com>
2026-03-21 17:13:17 -0300

    backup

--- src/main/java/br/com/fiap/dao/FuncionarioJuniorDao.java
+++ src/main/java/br/com/fiap/dao/FuncionarioJuniorDao.java
@@ -1,6 +1,5 @@
 package br.com.fiap.dao;
 
-import br.com.fiap.entity.Funcionario;
 import br.com.fiap.entity.FuncionarioJunior;
 import br.com.fiap.exception.CommitException;
 import br.com.fiap.exception.IdNaoEncontradoException;
